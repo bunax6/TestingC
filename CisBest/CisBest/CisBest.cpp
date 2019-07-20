58774f4896749a516040070d4767efb65aee4ddb
bunax6@gmail.com <bunax6@gmail.com>
2019-07-20 17:08:03 +0530

    Base version 1.0

---
+++ CisBest/CisBest/CisBest.cpp
@@ -0,0 +1,12 @@
+// CisBest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello bunax\n");
+	return 0;
+}
+
